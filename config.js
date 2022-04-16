ccf824f136a769c228fce9845fa14c33e4632570
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2022-04-16 13:11:15 -0500

    feat:init

---
+++ config.js
@@ -0,0 +1,26 @@
+/*
+getVideo - v1.0.0
+Sameera Damith
+damith.sameera1@gmail.com
+*/
+
+var config = {
+
+    //Application Title
+    title : "GetVideo: Download YouTube Videos Free.",
+  
+    //Error Page Title
+    errorTitle : "Page not Found (404) - GetVideo",
+  
+    //Application URL
+    url : "http://example.com",
+  
+    //Application Description
+    description : "Free online download YouTube videos, audio quickly in 1080p, 720p, MP4, 3GP, and more.",
+  
+    //Footer text
+    footerText : "FIVIP VIDEO DOWNLOADER &copy; 2022"
+  };
+  
+  module.exports = config;
+  
--- index.js
+++ index.js
@@ -1,28 +1,110 @@
 const express = require("express");
+const config = require("./config");
 const app = express();
 const ytdl = require("ytdl-core");
+const { title } = require("./config");
+const { param } = require("express/lib/request");
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-	return res.render("index");
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     next(createError(404));
+// });
+
+// error handler
+// app.use(function (err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error', { title: config.errorTitle, config: config });
+// });
+
+app.get("/", function (req, res, next) {
+  res.render("index", { title: config.title, config: config });
 });
 
-app.get("/download", async(req, res) => {
-	const v_id = req.query.url.split('v=')[1];
-    const info = await ytdl.getInfo(req.query.url);
-    console.log(info.formats[4]);
-    console.log(info.formats[1]);
+app.get("/download", async (req, res) => {
+  let v_id = req.query.url.split("v=")[1];
+  const info = await ytdl.getInfo(req.query.url);
+  console.log("FORMAT 4", info.formats[4]);
+  console.log("FORMAT 1", info.formats[1]);
 
-	return res.render("download", {
-		url: "https://www.youtube.com/embed/" + v_id,
-        info: info.formats.sort((a, b) => {
-            return a.mimeType < b.mimeType;
-        }),
-	});
+  let params = new URLSearchParams(req.query.url);
+  v = params.get('https://www.youtube.com/watch?v')
+
+  if(!!!v){
+      console.log(req.query.url)
+      v = req.query.url.split("/")[3]
+      v_id = req.query.url.split("/")[3]
+  }
+
+  console.log({params},{v});
+
+
+  return res.render("download", {
+    title: config.title,
+    config: config,
+    video_title: info.videoDetails.title,
+    imagen: `https://img.youtube.com/vi/${v}/sddefault.jpg`,
+    url: "https://www.youtube.com/embed/" + v_id,
+    info: info.formats.sort((a, b) => {
+      return a.mimeType < b.mimeType;
+    }),
+  });
 });
 
 
-app.listen(3000, () => {
-	console.log("Server is running on http://localhost:3000");
+app.get("/watch", async (req, res) => {
+
+    let v_id = req.query.v;
+
+    console.log(v_id);
+    const info = await ytdl.getInfo(`https://www.youtube.com/watch?v=${v_id}`);
+    console.log("FORMAT 4", info.formats[4]);
+    console.log("FORMAT 1", info.formats[1]);
+  
+    let params = new URLSearchParams(req.query.url);
+    v = v_id
+  
+    if(!!!v){
+        console.log(req.query.url)
+        v = req.query.url.split("/")[3]
+        v_id = req.query.url.split("/")[3]
+    }
+  
+    console.log({params},{v});
+  
+  
+    return res.render("download", {
+      title: config.title,
+      config: config,
+      video_title: info.videoDetails.title,
+      imagen: `https://img.youtube.com/vi/${v}/sddefault.jpg`,
+      url: "https://www.youtube.com/embed/" + v_id,
+      info: info.formats.sort((a, b) => {
+        return a.mimeType < b.mimeType;
+      }),
+    });
+  });
+
+app.get("/r", function (req, res, next) {
+  const { url, title, format } = req.query;
+
+  console.log({ url, title, format });
+  res.header("Content-Disposition", `attachement; filename=${title}.mp4`);
+
+  ytdl(url, {
+    quality: "highest",
+    format: "mp4",
+  }).pipe(res);
+});
+
+app.listen(3002, () => {
+  console.log("Server is running on http://localhost:3002");
 });
