5ae1b68c005f9838e396eb1be2e4f03934abaf3a
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2022-04-21 17:38:37 -0500

    feat:

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const fs = require("fs");
 const express = require("express");
 const config = require("./config");
 const app = express();
+const path = require('path');
 const ytdl = require("ytdl-core");
 const { title } = require("./config");
 const { param } = require("express/lib/request");
+const converter = require("./convertidor");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -36,16 +39,15 @@ app.get("/download", async (req, res) => {
   console.log("FORMAT 1", info.formats[1]);
 
   let params = new URLSearchParams(req.query.url);
-  v = params.get('https://www.youtube.com/watch?v')
+  v = params.get("https://www.youtube.com/watch?v");
 
-  if(!!!v){
-      console.log(req.query.url)
-      v = req.query.url.split("/")[3]
-      v_id = req.query.url.split("/")[3]
+  if (!!!v) {
+    console.log(req.query.url);
+    v = req.query.url.split("/")[3];
+    v_id = req.query.url.split("/")[3];
   }
 
-  console.log({params},{v});
-
+  console.log({ params }, { v });
 
   return res.render("download", {
     title: config.title,
@@ -59,40 +61,37 @@ app.get("/download", async (req, res) => {
   });
 });
 
-
 app.get("/watch", async (req, res) => {
+  let v_id = req.query.v;
 
-    let v_id = req.query.v;
+  console.log(v_id);
+  const info = await ytdl.getInfo(`https://www.youtube.com/watch?v=${v_id}`);
 
-    console.log(v_id);
-    const info = await ytdl.getInfo(`https://www.youtube.com/watch?v=${v_id}`);
+  // console.log("FORMAT 4", info.formats[4]);
+  // console.log("FORMAT 1", info.formats[1]);
 
-    // console.log("FORMAT 4", info.formats[4]);
-    // console.log("FORMAT 1", info.formats[1]);
-  
-    let params = new URLSearchParams(req.query.url);
-    v = v_id
-  
-    if(!!!v){
-        console.log(req.query.url)
-        v = req.query.url.split("/")[3]
-        v_id = req.query.url.split("/")[3]
-    }
-  
-    console.log({params},{v});
-  
-  
-    return res.render("download", {
-      title: config.title,
-      config: config,
-      video_title: info.videoDetails.title,
-      imagen: `https://img.youtube.com/vi/${v}/sddefault.jpg`,
-      url: "https://www.youtube.com/embed/" + v_id,
-      info: info.formats.sort((a, b) => {
-        return a.mimeType < b.mimeType;
-      }),
-    });
+  let params = new URLSearchParams(req.query.url);
+  v = v_id;
+
+  if (!!!v) {
+    console.log(req.query.url);
+    v = req.query.url.split("/")[3];
+    v_id = req.query.url.split("/")[3];
+  }
+
+  console.log({ params }, { v });
+
+  return res.render("download", {
+    title: config.title,
+    config: config,
+    video_title: info.videoDetails.title,
+    imagen: `https://img.youtube.com/vi/${v}/sddefault.jpg`,
+    url: "https://www.youtube.com/embed/" + v_id,
+    info: info.formats.sort((a, b) => {
+      return a.mimeType < b.mimeType;
+    }),
   });
+});
 
 app.get("/r", function (req, res, next) {
   const { url, title, format } = req.query;
@@ -106,6 +105,18 @@ app.get("/r", function (req, res, next) {
   }).pipe(res);
 });
 
+app.get("/c", async (req, res, next) => {
+
+  const { url, quality } = req.query;
+
+  // url = "https://www.youtube.com/watch?v=Tpv5ibeHdcE";
+  const file = await converter(url,quality);
+  var fileLocation = path.join('./',file);
+  res.download(fileLocation, file); 
+
+
+});
+
 app.listen(3003, () => {
-  console.log("Server is running on http://localhost:3002");
+  console.log("Server is running on http://localhost:3003");
 });
